eb8c7200a4c7a1610a55d9c968068f2467e76621
Ben Price <benpricedev@gmail.com>
2026-02-23 14:08:04 +0000

    Main.vue inital html structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <section class="content-wrapper" id="about"></section>
+    <section class="content-wrapper" id="experience"></section>
+    <section class="content-wrapper" id="education"></section>
+    <section class="content-wrapper" id="live-examples"></section>
+  </main>
 </template>
 
 <style scoped>
