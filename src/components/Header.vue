98c86eb50e8f1a94a4eb717fba24955f7773f612
Ben Price <benpricedev@gmail.com>
2026-02-23 13:59:40 +0000

    Header.vue link-wrapper visibility toggling functionality refactor

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
-import {type Ref, ref} from "vue";
+import {type Ref, ref, watch} from "vue";
+import {useCompLayoutState} from "@/stores/comp-layout-state.ts";
+import {storeToRefs} from "pinia";
 
-  const navLinkToggle:Ref = ref(false);
+  const breakPointStore = useCompLayoutState();
+  const {breakPointState} = storeToRefs(breakPointStore);
+  const navLinkVisibility:Ref = ref(false);
 
   function toggleNavLinks():void {
-    navLinkToggle.value = !navLinkToggle.value;
+    navLinkVisibility.value = !navLinkVisibility.value;
   }
-  //
-  // window.onresize = (e:Event) => {
-  //   if(navLinkToggle.value === false && window.innerWidth > 600){
-  //     navLinkToggle.value = true;
-  //   } else if (navLinkToggle.value === true && window.innerWidth < 600){
-  //     navLinkToggle.value = false;
-  //   }
-  // }
+
+  function setNavLinkVis(status:boolean):void {
+    navLinkVisibility.value = status;
+  }
+
+  function handleNavLinkBreakPoint(stateCheck:number):void {
+    if (stateCheck !== 0) {
+      setNavLinkVis(true);
+    } else {
+      setNavLinkVis(false);
+    }
+  }
+  watch(breakPointState, (newState) => {
+    handleNavLinkBreakPoint(newState);
+  })
 </script>
 
 <template>
@@ -24,7 +35,8 @@ import {type Ref, ref} from "vue";
     <span id="heading-wrapper">
       <h1>Heading</h1>
     </span>
-    <nav id="link-wrapper" v-if="navLinkToggle">
+
+    <nav id="link-wrapper" v-if="navLinkVisibility">
       <a class="head-link" >Link</a>
       <a class="head-link">Link</a>
       <a class="head-link">Link</a>
