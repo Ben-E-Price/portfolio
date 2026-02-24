001035dd9e983556144e6225d0885a59a253fe25
Ben Price <benpricedev@gmail.com>
2026-02-24 15:39:50 +0000

    Component Accordion created

---
+++ src/components/accordion/Accordion.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <details class="accordion">
+    <summary class="accordion-sum"></summary>
+    <p></p>
+  </details>
+</template>
+
+<style scoped>
+
+</style>
