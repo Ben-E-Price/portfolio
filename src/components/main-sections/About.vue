d4c5b18615817e05c22059d5e57a076f8043845d
Ben Price <benpricedev@gmail.com>
2026-02-24 13:28:17 +0000

    Destructuring id keys Removed variable declaration id

--- src/components/main-sections/About.vue
+++ src/components/main-sections/About.vue
@@ -2,7 +2,6 @@
   import List from "@/components/list/List.vue";
   import type {AboutData} from "@/types/content.ts";
 
-
   const {content} = defineProps<{content: AboutData}>();
   const {about, skills, contact} = content
   function validateAbout(data: any): data is AboutData {
@@ -19,19 +18,34 @@
   }
 
   validateAbout(content);
-  const id:string[] = Object.keys(content);
+  const [idAbout, idSkills, idContact]:string[] = Object.keys(content);
 </script>
 
 <template>
   <section class="content-wrapper">
     <div id="about">
-      <h2 id="about" v-to-heading>{{id[0]}}</h2>
-      <p>{{content.about}}</p>
+      <h2
+        :id="`heading-${idAbout}`"
+        v-to-heading>
+        {{idAbout}}
+      </h2>
+      <p>{{about}}</p>
     </div>
 
     <span>
-      <h2 id="skills" v-to-heading>{{id[1]}}</h2>
-      <List :items="content.skills" />
+      <h2
+        :id="`heading-${idSkills}`"
+        v-to-heading>
+        {{idSkills}}
+      </h2>
+      <List :items="skills" />
+    </span>
+
+    <span>
+      <h2 :id="`heading-${idContact}`"
+          v-to-heading>
+        {{idContact}}
+      </h2>
     </span>
   </section>
 </template>
--- src/directives/to-heading.ts
+++ src/directives/to-heading.ts
@@ -10,7 +10,8 @@ declare module 'vue' {
 
 export default {
   mounted: (el) => {
-    const text:string = el.innerText
-    el.textContent = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+    const text:string = el.innerText;
+    el.classList.add(`heading`);
+    el.textContent = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
   }
 } satisfies HeadingDirective
