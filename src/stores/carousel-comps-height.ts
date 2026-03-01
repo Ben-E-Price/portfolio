2429fe6c75cef79c56cfc8f0937e2393551419d7
Ben Price <benpricedev@gmail.com>
2026-03-01 20:49:53 +0000

    Class ElementHeight decalred contains element height finding functionality

--- src/stores/carousel-comps-height.ts
+++ src/stores/carousel-comps-height.ts
@@ -2,22 +2,31 @@ import {defineStore} from "pinia";
 
 type CarouselComps = "carousel-outer" | "carousel-controls" | "carousel-button";
 
+class ElementHeight {
+  readonly height: number;
+  private element: HTMLElement;
+
+  constructor(
+    private readonly elementName: string,
+    private readonly isId: boolean
+  ) {}
+
+  private getElement():this {
+    this.element:HTMLElement = this.isId ? document.getElementById(this.elementName) :
+      document.getElementsByClassName(this.elementName)[0];
+
+    return this;
+  }
+
+  private getHeight():this {
+    this.height = this.element.getBoundingClientRect().height;
+    return this;
+  }
+}
+
 export default defineStore("carousel-comps-height", () => {
   const _heightOuter:HTMLElement | null = document.getElementById("carousel-outer");
   const _heightControls:HTMLElement | null = document.getElementById("carousel-controls");
   const _heightButton:HTMLElement | null = document.getElementsByClassName("carousel-button")[0];
 
-  function getElementHeight(el:HTMLElement | undefined):number {
-    return el.getBoundingClientRect().height
-  }
-
-  function getElement(id:boolean, elName:CarouselComps){
-    const el:HTMLElement = id ? document.getElementById(elName) : document.getElementsByClassName(elName)[0]
-
-    if(el instanceof HTMLElement){
-      return el
-    } else {
-      console.error(`Can't find element with name ${elName}`);
-    }
-  }
 })
